17f9034a7f474da765787758ea6e880d8c4b6b87
d-plaindoux <d.plaindoux@free.fr>
2025-11-15 07:26:35 +0100

    Revision for Capitole du Libre

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -1,7 +1,18 @@
-
 #[cfg(test)]
 mod code_with_declarative {
     use declarative::foreach;
+
+    #[test]
+    pub fn should_perform_lists_foreach_with_filter() {
+        let r: Vec<_> = procedural::foreach! {
+            a <- (1..=3)
+            b <- (1..=a) if b != 2
+            yield (a, b)
+        }
+            .collect();
+
+        assert_eq!(r, vec![(1, 1), (2, 1), (3, 1), (3, 3)]);
+    }
 
     #[test]
     #[allow(unused_variables)]
@@ -23,9 +34,21 @@ mod code_with_procedural {
 
     #[test]
     pub fn should_perform_lists_foreach_with_filter() {
+        let r: Vec<_> = foreach! {
+            a <- (1..=3)
+            b <- (1..=a) if b != 2
+            yield (a, b)
+        }
+        .collect();
+
+        assert_eq!(r, vec![(1, 1), (2, 1), (3, 1), (3, 3)]);
+    }
+
+    #[test]
+    pub fn should_perform_option_foreach_with_filter() {
         let r: Option<i32> = foreach! {
-            a <- Some(1)
-            b <- None::<i32> if a != 2
+            a <- Some(2)
+            b <- Some(a) if a != 2
             c <- Some(3)
             yield a + b + c
         };
